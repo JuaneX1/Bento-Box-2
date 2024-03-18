4e36e4cbc0498ff371099746539bcdb552594b28
Rick Leinecker <penuelajuanesteban30@gmail.com>
2024-03-18 08:50:02 -0400

    changed for testing

--- frontend/src/components/SignUpForm.js
+++ frontend/src/components/SignUpForm.js
@@ -6,8 +6,8 @@ import logo from '../assets/FinalLogo.png';
 // comment and uncomment depending on where deploying on heroku
 // if this is our produnction, use Bento box 2
 // if this is our test, use Bento box 3
-const app_name = 'bento-box-2-df32a7e90651'
-//const app_name = 'bento-box-3-c00801a6c9a4'
+// const app_name = 'bento-box-2-df32a7e90651'
+const app_name = 'bento-box-3-c00801a6c9a4'
 
 // builds path if we local or if we are on heroku
 function buildPath(route)
