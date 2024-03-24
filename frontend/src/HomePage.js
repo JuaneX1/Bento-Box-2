7c97ecc33752903f44c350b62c5e14c44af6bd21
Nathaniel Gibson <89817052+gibs0nnn@users.noreply.github.com>
2024-03-24 12:52:04 -0400

    Login/Signup Forms
    Login and Signup forms will populate on new landing screen and can be switched inbetween.

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -3,22 +3,50 @@ import './HomePage.css';
 import AnimeList from './AnimeList';
 import AnimeInfo from './AnimeInfo';
 import bigLogo from './BB_Logo_Horizontal_COLOR_1.png';
-import Dashboard from './DashboardPage';
+import LoginForm from './LoginForm';
+import SignUpForm from './SignUpForm';
 
 const HomePage = () => {
     const [search, setSearch] = useState();
     const [animeData, setAnimeData] = useState();
     const [animeInfo, setAnimeInfo] = useState();
+    const [showLoginForm, setShowLoginForm] = useState(false);
+    const [showSignUpForm, setShowSignUpForm] = useState(false);
 
     const getData = async () => {
         const res = await fetch(`https://api.jikan.moe/v4/seasons/2024/spring`);
         const resData = await res.json();
         setAnimeData(resData.data);
-    }
+    };
 
     useEffect(() => {
         getData();
     }, [search]);
+
+    const handleLoginButtonClick = () => {
+        setShowLoginForm(true);
+        setShowSignUpForm(false);
+    };
+
+    const handleSignUpButtonClick = () => {
+        setShowSignUpForm(true);
+        setShowLoginForm(false);
+    };
+
+    const handleCloseForms = () => {
+        setShowLoginForm(false);
+        setShowSignUpForm(false);
+    };
+
+    const handleSwitchToLogin = () => {
+        setShowLoginForm(true);
+        setShowSignUpForm(false);
+    };
+
+    const handleSwitchToSignUp = () => {
+        setShowSignUpForm(true);
+        setShowLoginForm(false);
+    };
 
     return (
         <>
@@ -35,16 +63,24 @@ const HomePage = () => {
                         />
                     </div>
                 </div>
-                <div className="logo-container">
-                    <img src={bigLogo} alt="Big Logo" />
-                    <div className="buttons">
-                        <button className="button">Login</button>
-                        <button className="button">Sign Up</button>
-                    </div>
+                <div className="logo-and-form-container">
+                    {showLoginForm ? (
+                        <LoginForm onClose={handleCloseForms} onSwitchForm={handleSwitchToSignUp} />
+                    ) : showSignUpForm ? (
+                        <SignUpForm onClose={handleCloseForms} onSwitchBack={handleSwitchToLogin} />
+                    ) : (
+                        <div className="logo-container">
+                            <img src={bigLogo} alt="Big Logo" />
+                            <div className="buttons">
+                                <button className="button" onClick={handleLoginButtonClick}>Login</button>
+                                <button className="button" onClick={handleSignUpButtonClick}>Sign Up</button>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </>
     );
-}
+};
 
 export default HomePage;
