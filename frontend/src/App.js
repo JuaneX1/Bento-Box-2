371eedb2fd8fb9721a612f49a6adf410f2757115
Nathaniel Gibson <89817052+gibs0nnn@users.noreply.github.com>
2024-03-25 21:04:45 -0400

    Fixed routing issue
    Page was displaying twice because of two routes to it but I fixed it.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,19 +10,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './DashboardPage';
 import HomePage from './HomePage'; // Keep import for HomePage
 
-// All previously used components and states are commented out
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
-        </Routes>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-
       </div>
     </BrowserRouter>
   );
