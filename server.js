9dc735185bc6b4c2e4895e1ffc555acc7d539d06
Conner Harbaugh <savvydolphin77@gmail.com>
2024-03-31 16:41:20 -0400

    attempt 2

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ if (process.env.NODE_ENV === 'production') {
 	{
 		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 	});
-	// use https://bento-box-2-df32a7e90651.herokuapp.com/ for production
-	// use https://bento-box-3-c00801a6c9a4.herokuapp.com/ for testing
-    app.listen(PORT, 'https://bento-box-3-c00801a6c9a4.herokuapp.com/', () => {
-        console.log('Server listening on port ' + PORT);
-    });
+	// // use https://bento-box-2-df32a7e90651.herokuapp.com/ for production
+	// // use https://bento-box-3-c00801a6c9a4.herokuapp.com/ for testing
+    // app.listen(PORT, 'https://bento-box-3-c00801a6c9a4.herokuapp.com/', () => {
+    //     console.log('Server listening on port ' + PORT);
+    // });
 } else {
     // For local development
     app.listen(PORT, () => {
