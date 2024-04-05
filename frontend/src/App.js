b1618f711cafb8abaed6a51f57e47d78967f3a06
mon357luc <sonnywc@hotmail.com>
2024-04-04 22:27:39 -0400

    Profile Page update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<ProfilePage />} />
           <Route path="/verify/:token" element={<Verify />} />
 		  <Route path="/resetPassword/:token" element={<ResetPassword />} />
         </Routes>
