d338932e54ef8142a8e849d658fbfd5eb3f5cd00
ginobenitez <benitezgp81@gmail.com>
2024-04-07 01:08:53 -0400

    added storing token when logged in

--- bentoboxapp2/api/doLogin.js
+++ bentoboxapp2/api/doLogin.js
@@ -1,37 +1,38 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import { useState } from 'react';
 
 const PRODUCTION = true;
 
-export async function doLogin(formData, navigation){
-    //route.preventDefault();
-    if(formData.email != null || formData.password != null){
-        try {
-            
-            const response = await fetch(buildPath('api/login'), {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(formData)
-            });
-    
-            if (response.ok) {
+export async function doLogin(formData, navigation) {
+  // State for error handling
 
-                console.log(formData.email)
-                console.log('login success!')
-                navigation.navigate('Home');
-            } else {
-                // Handle incorrect login credentials
-                console.error('Incorrect email or password');
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
+  const instance = axios.create({
+    baseURL: 'https://bento-box-3-c00801a6c9a4.herokuapp.com/api',
+  });
+
+  if (formData.email !== null || formData.password !== null) {
+    try {
+      // Use await to wait for response from API call
+      const response = await instance.post(`/login`, formData);
+
+      // Handle successful login
+      if (response.status === 200) {
+        await AsyncStorage.setItem('token', response.data.token);
+       console.log('oou oou ouu!'); // Assuming this is the correct route
+        navigation.navigate('Home');
+      } else {
+        console.log('Unexpected response status:', response.status);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      // Handle network errors or other exceptions
+      setError('Network error or other issue. Please try again.');
     }
-    else{
-        console.log("invalid input");
-    }
+  } else {
+    console.log("Invalid input");
+  }
 }
-
 function buildPath(route)
 {
     //const app_name = 'bento-box-mobile-c040aef8aea0'
