88d4f6aa55674df4ce47f5f668a0eb8982a6a280
Rick Leinecker <penuelajuanesteban30@gmail.com>
2024-04-14 10:41:54 -0400

    Fixed Anime Card Styling
    To keep consistency between the browse page and home page, styles have been unified, and unused code has been removed.

--- frontend/src/components/animeCards/AnimeCard.js
+++ frontend/src/components/animeCards/AnimeCard.js
@@ -1,20 +1,44 @@
-import React from 'react'
-import '../../css/AnimeCard.css'
+import React, { useState } from 'react';
+import '../../css/AnimeCard.css';
 
-function AnimeCard({anime}) {
-    return (
-        <article className="anime-card">
-            <a
-                href={anime.url}
-                target ="_blank"
-                rel="norefererr">
-                <figure>
-                    <img src={anime.images.jpg.image_url} alt="AnimeImage" />
-                </figure>
-                <h3>{ anime.title }</h3>
-            </a>
-        </article>
-    )
+function AnimeCard({ anime }) {
+  const [showSynopsis, setShowSynopsis] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowSynopsis(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowSynopsis(false);
+  };
+
+  const truncateSynopsis = (synopsis, maxLength) => {
+    if (synopsis.length <= maxLength) {
+      return synopsis;
+    }
+    return synopsis.substring(0, maxLength) + '...';
+  };
+
+  return (
+    <article className="anime-card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+    <a
+        href={anime.url}
+        target="_blank"
+        rel="norefererr"
+      >
+        <figure>
+          <img src={anime.images.jpg.image_url} alt="AnimeImage" />
+        </figure>
+        <h3>{anime.title}</h3>
+
+      {showSynopsis && (
+        <div className="synopsis-overlay">
+          <p>{truncateSynopsis(anime.synopsis, 150)}</p>
+        </div>
+      )}
+    </a>
+    </article>
+  );
 }
 
-export default AnimeCard
+export default AnimeCard;
--- frontend/src/components/animeCards/AnimeList.js
+++ frontend/src/components/animeCards/AnimeList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AnimeCard from './AnimeCard'
 import '../../css/AnimeList.css';
 
 const AnimeList = ({ animelist, setAnimeInfo }) => {
@@ -6,20 +7,10 @@ const AnimeList = ({ animelist, setAnimeInfo }) => {
     <div className="anime-list">
       {animelist ? (
         animelist.map((anime, index) => (
-          <div className="card" key={index} >
-            <img src={anime.images.jpg.large_image_url} alt="animeImage" />
-            <div className="anime-info">
-              <div className="title">
-                <h4>{anime.title}</h4>
-              </div>
-              <div className="synopsis">
-                <p>{anime.synopsis.length > 150 ? anime.synopsis.substring(0, 150) + "..." : anime.synopsis}</p>
-              </div>
-            </div>
-          </div>
+          <AnimeCard anime= {anime} key={index} setAnimeInfo={setAnimeInfo} />
         ))
       ) : (
-        <p>Anime not found</p>
+        <p>loading...</p>
       )}
     </div>
   );
