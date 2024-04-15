198ec8bd9301da8f760c957ebee92ef536d8e115
Conner Harbaugh <savvydolphin77@gmail.com>
2024-04-14 22:25:57 -0400

    Removed Hover Info on Home Page Cards
    Removed information on home page, can still hover over it, but doesn't display information as that is a purely decorative design feature.

--- frontend/src/components/animeCards/AnimeList.js
+++ frontend/src/components/animeCards/AnimeList.js
@@ -8,14 +8,6 @@ const AnimeList = ({ animelist, setAnimeInfo }) => {
         animelist.map((anime, index) => (
           <div className="card" key={index} >
             <img src={anime.images.jpg.large_image_url} alt="animeImage" />
-            <div className="anime-info">
-              <div className="title">
-                <h4>{anime.title}</h4>
-              </div>
-              <div className="synopsis">
-                <p>{anime.synopsis.length > 150 ? anime.synopsis.substring(0, 150) + "..." : anime.synopsis}</p>
-              </div>
-            </div>
           </div>
         ))
       ) : (
