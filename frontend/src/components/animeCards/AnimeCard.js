79b674c32d622089dcb4bc2f151919b316ed972f
Conner Harbaugh <savvydolphin77@gmail.com>
2024-04-21 23:15:14 -0400

    Updated Anime Cards to Use Bootstrap
    Updated anime cards to use bootstrap

--- frontend/src/components/animeCards/AnimeCard.js
+++ frontend/src/components/animeCards/AnimeCard.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../../css/AnimeCard.css';
 
 function AnimeCard({ anime }) {
   const [showSynopsis, setShowSynopsis] = useState(false);
@@ -13,29 +12,43 @@ function AnimeCard({ anime }) {
     setShowSynopsis(false);
   };
 
+  const handleBlur = () => {
+    setShowSynopsis(false);
+  };
+
   const truncateSynopsis = (synopsis) => {
     if (!synopsis || synopsis.length <= 150) {
       return synopsis;
     }
     return synopsis.substring(0, 150) + '...';
   };
-  
-  
-  return (
-    <article className="anime-card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      <Link to={`/anime/${anime.mal_id}`} state={{ anime }}>
-        <figure>
-          <img src={anime.images.jpg.image_url} alt="AnimeImage" />
-        </figure>
-        <h3>{anime.title_english}</h3>
 
+  return (
+    <div className="card anime-card card overflow-hidden border border-dark" style={{ objectFit: "cover", minWidth: '250px', maxWidth: '250px'}}>
+      <div
+        className="card-img-wrapper"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onFocus={handleMouseEnter}
+        onBlur={handleBlur}
+      >
+        <Link
+          to={`/anime/${anime.mal_id}`}
+          state={{ anime }}
+          className="text-decoration-none text-dark"
+        >
+          <img src={anime.images.jpg.image_url} className="card-img-top" alt="AnimeImage" />
+          <div className="title-overlay position-absolute start-0 bottom-0 w-100 p-2 bg-black text-white">
+            <h5 className="card-title text-white text-center mb-0" style={{ fontSize: '14px' }}>{anime.title_english}</h5>
+          </div>
+        </Link>
         {showSynopsis && (
-          <div className="synopsis-overlay">
-            <p>{truncateSynopsis(anime.synopsis)}</p>
+          <div className="synopsis-overlay position-absolute start-0 top-0 w-100 h-100 bg-black opacity-75 d-flex flex-column justify-content-center align-items-center">
+            <p className="card-text text-white text-center p-2">{truncateSynopsis(anime.synopsis)}</p>
           </div>
         )}
-      </Link>
-    </article>
+      </div>
+    </div>
   );
 }
 
