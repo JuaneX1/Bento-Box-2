11548c9c32a197d5b91a9ba30ba0ae8554c36cfe
Conner Harbaugh <savvydolphin77@gmail.com>
2024-04-21 20:39:21 -0400

    Updated Styles Profile, Fixed Spacing & Navbar
    Fixed some spacing and navbar issues in bootstrap for the profile page

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Container, Form, Image, Modal } from 'react-bootstrap';
+import { Button, Container, Form, Modal } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import { instance } from '../App';
 import logo from '../assets/FinalLogo.png';
-
+import styled from 'styled-components';
 
 const ProfilePage = ({ onClose }) => {
     const [userData, setUserData] = useState('');
@@ -70,53 +70,68 @@ const ProfilePage = ({ onClose }) => {
         navigate('/');
     };
 
+    const TopNavbar = styled.nav`
+        background-color: #111920; 
+    `;
+
     return (
         <>
-            <header className="topbar">
-              <Link to="/dashboard">
-                <Image src={logo} alt="Logo" className="topbar-logo" fluid />
-              </Link>
-              <div className="topbar-buttons">
-                  <Link to="/dashboard" className="topbar-btn btn btn-primary">Dashboard</Link>
-                  <Button onClick={handleLogOut} className="log-out-btn btn btn-danger">Log Out</Button>
-              </div>
-            </header>
-            {/* the 89.20 is to ensure gradient doesn't repeat in container*/}
-            <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '89.20vh' }}>
-            <Container className="text-white">
-                <div className="profile-content border-whiteborder border-secondary bg-black p-4 rounded">
-                    <h1 className="mb-4">My Profile</h1>
-                    {isEditing ? (
-                        <Form onSubmit={handleSubmit} className="profile-form text-light align-labels-left">
-                            <Form.Group className="mb-3" controlId="formFirstName">
-                                <Form.Label>First Name</Form.Label>
-                                <Form.Control type="text" name="first" value={userData.first} onChange={handleChange} />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formLastName">
-                                <Form.Label>Last Name</Form.Label>
-                                <Form.Control type="text" name="last" value={userData.last} onChange={handleChange} />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="formUsername">
-                                <Form.Label>Username</Form.Label>
-                                <Form.Control type="text" name="login" value={userData.login} onChange={handleChange} />
-                            </Form.Group>
-                            {/* Update Profile Button */}
-                            <Button type="submit" className="mt-4 profile-btn btn btn-success">Update Profile</Button>
-                        </Form>
-                    ) : (
-                        <div className="profile-info flex justify-items-left">
-                            <p className='d-flex align-items-start'><strong>First Name:</strong> <div className='p-1'> </div><span>{userData.first}</span> </p>
-                            <p className='d-flex align-items-start'><strong>Last Name:</strong> <div className='p-1'></div>{userData.last}</p>
-                            <p className='d-flex align-items-start'><strong>Username:</strong> <div className='p-1'></div>{userData.login}</p>
-                            <p className='d-flex align-items-start'><strong>Email:</strong><div className='p-1'></div> {userData.email}</p>
-                            {/* Edit Profile Button */}
-                            <Button onClick={handleToggleEdit} className="edit-profile-btn btn btn-primary">Edit Profile</Button>
-                        </div>
-                    )}
-                    {/* Delete Account Button */}
-                    <Button onClick={() => setShowModal(true)} className="mt-4 delete-account-btn btn btn-danger">Delete Account</Button>
+            <TopNavbar className="navbar navbar-expand-lg navbar-dark d-flex justify-content-between p-2">
+                <div className="container-fluid">                
+                    <Link to="/dashboard" className="navbar-brand">
+                        <strong>Back to Anime</strong>
+                    </Link>
+                    <Link to="/dashboard" className="navbar-brand ml-auto">
+                        <img src={logo} alt="Big Logo" className="logo img-fluid mr-3" style={{ minHeight: '50px', maxHeight: '50px' }} />
+                    </Link>
+                    <div className="ml-auto">
+                        <Button onClick={handleLogOut} className="btn btn-danger">
+                            <strong>Log Out</strong>
+                        </Button>
+                    </div>
                 </div>
-            </Container>
+            </TopNavbar>
+            <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '89.75vh', background: "linear-gradient(to bottom, #2e77AE, #000000)" }}>
+                <Container className="text-white" style={{ maxWidth: '500px' }}>
+                    <div className="profile-content border-white border-secondary bg-black p-4 rounded">
+                        <h1 className="mb-4">My Profile</h1>
+                        {isEditing ? (
+    <Form onSubmit={handleSubmit} className="profile-form text-light align-labels-left">
+        <Form.Group className="mb-3" controlId="formFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" name="first" value={userData.first} onChange={handleChange} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="text" name="last" value={userData.last} onChange={handleChange} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" name="login" value={userData.login} onChange={handleChange} />
+        </Form.Group>
+        {/* Cancel Button */}
+        <div className='d-flex justify-content-start'>
+            <Button onClick={handleToggleEdit} className="mt-4 p-2 btn btn-secondary mr-2">Stop Editing</Button>
+            <Container style={{ maxWidth: "1px", margin: "0px"}}></Container>
+            {/* Update Profile Button */}
+            <Button type="submit" className="mt-4 p-2 profile-btn btn btn-success">Update Profile</Button>
+        </div>
+    </Form>
+) : (
+    <div className="profile-info flex justify-items-left">
+        <p className='d-flex align-items-start'><strong>First Name:</strong> <div className='p-1'> </div><span>{userData.first}</span> </p>
+        <p className='d-flex align-items-start'><strong>Last Name:</strong> <div className='p-1'></div>{userData.last}</p>
+        <p className='d-flex align-items-start'><strong>Username:</strong> <div className='p-1'></div>{userData.login}</p>
+        <p className='d-flex align-items-start'><strong>Email:</strong><div className='p-1'></div> {userData.email}</p>
+        {/* Edit Profile Button */}
+        <Button onClick={handleToggleEdit} className="edit-profile-btn btn btn-primary">Edit Profile</Button>
+    </div>
+)}
+
+                        {/* Delete Account Button */}
+                        <Button onClick={() => setShowModal(true)} className="mt-4 delete-account-btn btn btn-danger">Delete Account</Button>
+                    </div>
+                </Container>
             </div>
             {showModal && <div className="overlay"></div>}
             {/* Modal for confirming account deletion */}
