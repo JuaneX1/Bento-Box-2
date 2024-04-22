bec39c90aca4460285f66dd18f9715f11be80642
Conner Harbaugh <savvydolphin77@gmail.com>
2024-04-21 22:48:30 -0400

    Added Notification Profile Updated
    Added notification profile is updated

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -12,6 +12,7 @@ const ProfilePage = ({ onClose }) => {
     const [showModal, setShowModal] = useState(false);
     const [isEditing, setIsEditing] = useState(false);
     const navigate = useNavigate();
+    const [showUpdateProfile, setShowUpdatedProfile] = useState(false);
     
     useEffect(() => {
         const getUserInfo = async () => {
@@ -38,6 +39,7 @@ const ProfilePage = ({ onClose }) => {
 
     const handleToggleEdit = () => {
         setIsEditing(prevState => !prevState);
+        setShowUpdatedProfile(false);
         if (!isEditing) {
             // Reset user data to original values when toggling edit mode off
             setUserData(originalUserData);
@@ -50,6 +52,7 @@ const ProfilePage = ({ onClose }) => {
         await instance.patch(`/updateInfo`, userData, { headers: { Authorization: sessionStorage.getItem('token') }} ).then( response => {
             sessionStorage.setItem('token', response.data.token);
             setIsEditing(false); // Disable edit mode after successful update
+            setShowUpdatedProfile(true);
         }).catch( error => {
             console.log(error);
             setError( error );
@@ -59,7 +62,7 @@ const ProfilePage = ({ onClose }) => {
     const handleDeleteAccount = async () => {
         try {
             await instance.delete(`/deleteUser`, { headers: { Authorization: sessionStorage.getItem('token') }});
-            navigate('/');
+            navigate('/?action=delete');
         } catch (error) {
             setError(error.response.data.error);
         }
@@ -91,47 +94,56 @@ const ProfilePage = ({ onClose }) => {
                     </div>
                 </div>
             </TopNavbar>
-            <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '89.75vh', background: "linear-gradient(to bottom, #2e77AE, #000000)" }}>
+            <div style={{ minHeight: '89.75vh', background: "linear-gradient(to bottom, #2e77AE, #000000)" }}>
+            {showUpdateProfile && (
+                    <div className="verification-bar bg-success text-white p-2">
+                        <p className="m-0 text-center">
+                            Profile Successfully Updated!
+                        </p>
+                    </div>
+                )}
+                <div className='p-5 d-flex justify-content-center align-items-center'>
                 <Container className="text-white" style={{ maxWidth: '500px' }}>
                     <div className="profile-content border-white border-secondary bg-black p-4 rounded">
                         <h1 className="mb-4">My Profile</h1>
                         {isEditing ? (
-    <Form onSubmit={handleSubmit} className="profile-form text-light align-labels-left">
-        <Form.Group className="mb-3" controlId="formFirstName">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control type="text" name="first" value={userData.first} onChange={handleChange} />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formLastName">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control type="text" name="last" value={userData.last} onChange={handleChange} />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formUsername">
-            <Form.Label>Username</Form.Label>
-            <Form.Control type="text" name="login" value={userData.login} onChange={handleChange} />
-        </Form.Group>
-        {/* Cancel Button */}
-        <div className='d-flex justify-content-start'>
-            <Button onClick={handleToggleEdit} className="mt-4 p-2 btn btn-secondary mr-2">Stop Editing</Button>
-            <Container style={{ maxWidth: "1px", margin: "0px"}}></Container>
-            {/* Update Profile Button */}
-            <Button type="submit" className="mt-4 p-2 profile-btn btn btn-success">Update Profile</Button>
-        </div>
-    </Form>
-) : (
-    <div className="profile-info flex justify-items-left">
-        <p className='d-flex align-items-start'><strong>First Name:</strong> <div className='p-1'> </div><span>{userData.first}</span> </p>
-        <p className='d-flex align-items-start'><strong>Last Name:</strong> <div className='p-1'></div>{userData.last}</p>
-        <p className='d-flex align-items-start'><strong>Username:</strong> <div className='p-1'></div>{userData.login}</p>
-        <p className='d-flex align-items-start'><strong>Email:</strong><div className='p-1'></div> {userData.email}</p>
-        {/* Edit Profile Button */}
-        <Button onClick={handleToggleEdit} className="edit-profile-btn btn btn-primary">Edit Profile</Button>
-    </div>
-)}
+                            <Form onSubmit={handleSubmit} className="profile-form text-light align-labels-left">
+                                <Form.Group className="mb-3" controlId="formFirstName">
+                                    <Form.Label>First Name</Form.Label>
+                                    <Form.Control type="text" name="first" value={userData.first} onChange={handleChange} />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formLastName">
+                                    <Form.Label>Last Name</Form.Label>
+                                    <Form.Control type="text" name="last" value={userData.last} onChange={handleChange} />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formUsername">
+                                    <Form.Label>Username</Form.Label>
+                                    <Form.Control type="text" name="login" value={userData.login} onChange={handleChange} />
+                                </Form.Group>
+                                {/* Cancel Button */}
+                                <div className='d-flex justify-content-start'>
+                                    <Button onClick={handleToggleEdit} className="mt-4 p-2 btn btn-secondary mr-2">Stop Editing</Button>
+                                    <Container style={{ maxWidth: "1px", margin: "0px"}}></Container>
+                                    {/* Update Profile Button */}
+                                    <Button type="submit" className="mt-4 p-2 profile-btn btn btn-success">Update Profile</Button>
+                                </div>
+                            </Form>
+                        ) : (
+                            <div className="profile-info flex justify-items-left">
+                                <p className='d-flex align-items-start'><strong>First Name:</strong> <div className='p-1'> </div><span>{userData.first}</span> </p>
+                                <p className='d-flex align-items-start'><strong>Last Name:</strong> <div className='p-1'></div>{userData.last}</p>
+                                <p className='d-flex align-items-start'><strong>Username:</strong> <div className='p-1'></div>{userData.login}</p>
+                                <p className='d-flex align-items-start'><strong>Email:</strong><div className='p-1'></div> {userData.email}</p>
+                                {/* Edit Profile Button */}
+                                <Button onClick={handleToggleEdit} className="edit-profile-btn btn btn-primary">Edit Profile</Button>
+                            </div>
+                        )}
 
                         {/* Delete Account Button */}
                         <Button onClick={() => setShowModal(true)} className="mt-4 delete-account-btn btn btn-danger">Delete Account</Button>
                     </div>
                 </Container>
+                </div>
             </div>
             {showModal && <div className="overlay"></div>}
             {/* Modal for confirming account deletion */}
