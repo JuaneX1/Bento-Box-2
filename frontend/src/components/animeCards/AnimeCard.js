20d5108fd53fcca3fe5bd19d3696eadebcdb5274
Conner Harbaugh <savvydolphin77@gmail.com>
2024-04-22 00:07:42 -0400

    Moved Linked on Cards So Card Clickable
    Fixed link not being able to be clicked to take to anime page by changing link location.

--- frontend/src/components/animeCards/AnimeCard.js
+++ frontend/src/components/animeCards/AnimeCard.js
@@ -32,20 +32,22 @@ function AnimeCard({ anime }) {
         onFocus={handleMouseEnter}
         onBlur={handleBlur}
       >
-        <Link
-          to={`/anime/${anime.mal_id}`}
-          state={{ anime }}
-          className="text-decoration-none text-dark"
-        >
+     
           <img src={anime.images.jpg.image_url} className="card-img-top" alt="AnimeImage" />
           <div className="title-overlay position-absolute start-0 bottom-0 w-100 p-2 bg-black text-white">
             <h5 className="card-title text-white text-center mb-0" style={{ fontSize: '14px' }}>{anime.title_english}</h5>
           </div>
-        </Link>
+       
         {showSynopsis && (
+          <Link
+             to={`/anime/${anime.mal_id}`}
+             state={{ anime }}
+             className="text-decoration-none text-dark"
+           >
           <div className="synopsis-overlay position-absolute start-0 top-0 w-100 h-100 bg-black opacity-75 d-flex flex-column justify-content-center align-items-center">
             <p className="card-text text-white text-center p-2">{truncateSynopsis(anime.synopsis)}</p>
           </div>
+          </Link>
         )}
       </div>
     </div>
