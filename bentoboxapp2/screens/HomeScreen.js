014e1dc954271968d93b599c4b7cb616acd54e11
ginobenitez <benitezgp81@gmail.com>
2024-04-25 14:23:43 -0400

    Update HomeScreen.js

--- bentoboxapp2/screens/HomeScreen.js
+++ bentoboxapp2/screens/HomeScreen.js
@@ -78,7 +78,7 @@ export default function HomeScreen({ navigation }) {
 
   useEffect(() => {
     fetchData();
-  }, [fetchData]);
+  }, [handleRefresh]);
 
   const handleRefresh = () => {
     fetchData(); // Directly invoke fetching data
